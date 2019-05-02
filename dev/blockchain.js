60b8221df741c0088ae8fc1f2b2255bdebc79678
martineng <martin.eca.6651@gmail.com>
2019-05-02 17:47:14 +0800

    Added proofOfWork

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -23,14 +23,14 @@ function Blockchain(){
 
    Blockchain.prototype.getLastBlock = function(){
       return this.chain[this.chain.length - 1];
-   }
+   } // END getLastBlock
 
    Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
       const newTransaction = {
          amount: amount,
          sender: sender,
          recipient: recipient,
-      }
+      } // END createNewTransaction
 
       this.newTransaction.push(newTransaction);
       return this.getlastBlock()['index'] + 1;
@@ -43,9 +43,23 @@ function Blockchain(){
       const hash = sha256(dataString);
       
       return hash;
-   }
+   } // END hashBlock
+
+   Blockchian.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
+      // Using let as our variables will be changing through the process
+      let nonce =0;
+      let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+      // If the generated hash doesn't start with 4 zero, hash again with different nonce.
+      while (hash.substring(0, 4) != '0000'){
+         nonce++;
+         hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+      }
+
+      return nonce;
+   } // END proofOfWork
 
 
-} // END Blockchain()
+} // END Blockchain
 
 module.exports = Blockchain;
