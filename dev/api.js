fee9fdd280b404cff42ca8473eced5baa638a341
martineng <martin.eca.6651@gmail.com>
2019-05-07 16:38:42 +0800

    Added comments in API

--- dev/api.js
+++ dev/api.js
@@ -2,43 +2,47 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
-const bitcoin = new Blockchain();
 const uuid = require('uuid/v1');
+
+const bitcoin = new Blockchain();
 const nodeAddress = uuid().split('-').join('');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Front Page
 app.get('/', function(req, res){
-   res.send('Hello Friend');
+   res.send('Basic Blockchain written in Javascript');
 });
 
+// Send the entire Blockchain to endpoint
 app.get('/blockchain', function(req, res){
    res.send(bitcoin);
 });
 
+// Send the Transaction result in blockIndex
 app.post('/transaction', function(req, res){
-   //console.log(req.body);
-   //res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
-
    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
    res.json({ note: `Transaction will be added in block ${blockIndex}.`});
 });
 
+// Mine and send new block info
 app.get('/mine', function(req, res){
-
+   // Get the previous hash
    const lastBlock = bitcoin.getLastBlock();
    const previousBlockHash = lastBlock['hash'];
 
+   // Current block consists Transactions and index of this block
    const currentBlockData = {
       transactions: bitcoin.pendingTransactions,
       index: lastBlock['index'] + 1,
    };
 
+   // Using proofOfWork and previous & current node to generate nocne
    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
 
+   // Create hash
    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
-
    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
 
    res.json({
@@ -46,6 +50,7 @@ app.get('/mine', function(req, res){
       block: newBlock,
    });
 
+   // Reward for miner who found and create this new block
    bitcoin.createNewTransaction(12.5, "00", nodeAddress);
 
 });
