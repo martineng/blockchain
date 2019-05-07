6594b4f591443e8fa3af0f8e1230a216b55f0c29
martineng <martin.eca.6651@gmail.com>
2019-05-07 16:12:43 +0800

    Tested API for blockchain, transaction and mine end-points

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 ## A Simple Blockchain written in Javascript using Node.js
 The pupose of me starting this project is to demonstrate the implementation of Blockchain technology and to help the community understand this revolutionary tech in a practicle way.
 
-### Installation
+### Installed Libraries
 npm i sha256 --save    //sha256 Hashing
 npm i express --save   //express.js library
 npm i nodemon --save   //nodemon library restart our server automatically when we make changes 
 npm i body-parser --save
+npm i uuid --save
--- dev/api.js
+++ dev/api.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
+const uuid = require('uuid/v1');
+const nodeAddress = uuid().split('-').join('');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,12 +18,36 @@ app.get('/blockchain', function(req, res){
 });
 
 app.post('/transaction', function(req, res){
-   console.log(req.body);
+   //console.log(req.body);
+   //res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
 
-   res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
+   const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+   res.json({ note: `Transaction will be added in block ${blockIndex}.`});
 });
 
 app.get('/mine', function(req, res){
+
+   const lastBlock = bitcoin.getLastBlock();
+   const previousBlockHash = lastBlock['hash'];
+
+   const currentBlockData = {
+      transactions: bitcoin.pendingTransactions,
+      index: lastBlock['index'] + 1,
+   };
+
+   const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+
+   const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+   const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+
+   res.json({
+      note: "New block mined successfully",
+      block: newBlock,
+   });
+
+   bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+
 });
 
 // Serve is listening to port 3000
