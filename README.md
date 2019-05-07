a73dbb4d96a9ad63c9871dc9da1b10a9f5960ac5
martineng <martin.eca.6651@gmail.com>
2019-05-07 12:31:44 +0800

    Started to build an api

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ## A Simple Blockchain written in Javascript using Node.js
 The pupose of me starting this project is to demonstrate the implementation of Blockchain technology and to help the community understand this revolutionary tech in a practicle way.
+
+### Installation
+npm i sha256 --save    //sha256 Hashing
+npm i express --save   //express.js library
+npm i nodemon --save   //nodemon library restart our server automatically when we make changes 
+npm i body-parser --save
--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,30 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
-   res.send('Hello World');
+   res.send('Hello Friend');
 });
 
-app.listen(3000);
+app.get('/blockchain', function(req, res){
+   res.send(bitcoin);
+});
+
+app.post('/transaction', function(req, res){
+   console.log(req.body);
+
+   res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
+});
+
+app.get('/mine', function(req, res){
+});
+
+// Serve is listening to port 3000
+app.listen(3000, function(){
+   console.log('listening on port 3000...');
+}); 
